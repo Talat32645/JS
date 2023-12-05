caea1bcc12ce96238fff4c8845f760a22702a058
Talat32645 <151937250+Talat32645@users.noreply.github.com>
2023-12-05 14:21:39 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,30 @@
+var user = prompt ('Введите своё имя')
+
+console.log ( ' Ваше имя ' + user );
+
+
+var year = +prompt ('Введите свой возраст')
+
+console.log ( ' Вам ' + year );
+
+var p1 = +prompt ('Пример')
+
+console.log ( ' Пример ' + ' 6 + 3 = ' (6 + 3) + 'Ваш ответ' + p1 );
+
+var p2 = +prompt ('Пример')
+
+console.log ( ' Пример ' + ' 20 - 15 = ' (20 - 15) + 'Ваш ответ' + p2 );
+
+var p3 = +prompt ('Пример')
+
+console.log ( ' Пример ' + ' 15 * 2 = ' (15 * 2) + 'Ваш ответ' + p3 );
+
+var p4 = +prompt ('Пример')
+
+console.log ( ' Пример ' + ' 8 / 4 = ' (8 / 4) + 'Ваш ответ' + p4 );
+
+
+var p5 = +prompt ('Пример')
+
+console.log ( ' Пример ' + ' 10 % 3 = ' (10 % 3) + 'Ваш ответ' + p5 );
+
